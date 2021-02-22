af2d6fc76fe86dad0c1b76ac232a0d76c29a9b5e
Divyosmi Goswami <goswamidivyosmi@gmail.com>
2021-02-22 21:47:39 +0530

    Update App.js

--- App/App.js
+++ App/App.js
@@ -3,8 +3,7 @@ import { Text, View, StyleSheet, Image, Linking } from 'react-native';
 import Constants from 'expo-constants';
 
 // You can import from local files
-import AssetExample from './components/AssetExample';
-
+// You can import from local files
 // or any pure javascript modules available in npm
 import { Card } from 'react-native-paper';
 
